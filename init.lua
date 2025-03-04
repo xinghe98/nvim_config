10d7076d407f48d624cb228297889ec34c7d5de0
xinghe98 <lcg9899@gmail.com>
2025-03-04 21:04:54 +0800

    init

--- init.lua
+++ init.lua
@@ -14,3 +14,36 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmd")
 require('telescope').load_extension('coc')
+vim.g.coc_global_extensions = {
+	"coc-marketplace",
+	"coc-word",
+	"coc-css",
+	"coc-highlight",
+	"coc-sumneko-lua",
+	"coc-go",
+	"coc-diagnostic",
+	"coc-docker",
+	-- "coc-eslint",
+	"coc-flutter",
+	"coc-gitignore",
+	"coc-git",
+	"coc-html",
+	"coc-jest",
+	"coc-json",
+	"coc-lists",
+	"coc-omnisharp",
+	"coc-prettier",
+	"coc-prisma",
+	"coc-snippets",
+	"https://github.com/rafamadriz/friendly-snippets@main",
+	-- "coc-syntax",
+	"coc-word",
+	"coc-emoji",
+	"coc-tsserver",
+	"@yaegassy/coc-volar",
+	"coc-emmet",
+	"coc-yaml",
+	"coc-yank",
+	"coc-pyright",
+	"coc-symbol-line"
+}
