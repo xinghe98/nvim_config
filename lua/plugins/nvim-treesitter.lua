257b3a51f90c112e6a8b9864cb420628295d5fa7
xinghe98 <lcg9899@gmail.com>
2025-05-09 11:31:37 +0800

    u

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup {
 			ignore_install = { "help" },
-			ensure_installed = { "vim", "vimdoc", "go", "python", "typescript", "markdown", "markdown_inline", "query", "c", "vim", "html", "vue", "lua", "dart" },
+			ensure_installed = { "vim", "vimdoc", "go", "python", "typescript", "query", "c", "vim", "html", "vue", "lua", "dart" },
 			highlight = {
 				enable = true,
 			},
