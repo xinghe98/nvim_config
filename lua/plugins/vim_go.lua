6a21531eea9cdfaf2dec377c9c5332428dac0ef7
xinghe98 <lcg9899@gmail.com>
2025-05-11 09:11:55 +0800

    1

--- lua/plugins/vim_go.lua
+++ lua/plugins/vim_go.lua
@@ -5,8 +5,8 @@ return {
 	config = function()
 		vim.cmd("au FileType go nmap <Leader>gd <Plug>(go-doc)")
 		vim.g.go_doc_keywordprg_enabled = 0
-		vim.g.go_gopls_enabled          = 1
-		vim.g.go_echo_go_info           = 0
+		vim.g.go_gopls_enabled          = 0
+		vim.g.go_echo_go_info           = 1
 		vim.g.go_doc_popup_window       = 1
 		vim.g.go_def_mapping_enabled    = 0
 		vim.g.go_template_autocreate    = 0
