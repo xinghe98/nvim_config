37c16547bd383d4d2eff344d9948fa84e3088f4c
xinghe98 <lcg9899@gmail.com>
2025-07-24 20:48:53 +0800

    l

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,17 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = "VimEnter",
+	branch = 'master',
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup {
+		require 'nvim-treesitter.configs'.setup {
 			ignore_install = { "help" },
 			ensure_installed = { "vim", "vimdoc", "go", "python", "typescript", "query", "c", "vim", "html", "vue", "lua", "dart" },
 			highlight = {
 				enable = true,
 			},
 		}
+		-- 确保语法高亮开启
+		vim.cmd("syntax enable")
 	end
 }
