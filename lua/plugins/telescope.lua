01a255c120e186bac995abd41e5b0f17ae255aa1
xinghe98 <lcg9899@gmail.com>
2025-07-31 10:32:35 +0800

    u

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -46,9 +46,12 @@ return {
 		},
 		extensions = {
 			coc = {
-				theme = 'dropdown',
+				theme = 'ivy',
 				prefer_locations = true, -- always use Telescope locations to preview definitions/declarations/implementations etc
+				push_cursor_on_edit = true, -- save the cursor position to jump back in the future
+				timeout = 300,  -- timeout for coc commands
 			}
+
 		},
 	},
 }
